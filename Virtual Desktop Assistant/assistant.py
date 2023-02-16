357fc5198cf832a9700961d29ad0db8034a859a4
Sahil Sandi <officialsahilsandi@gmail.com>
2023-02-16 19:30:55 +0530

    Virtual Desktop Assistant

--- Virtual Desktop Assistant/assistant.py
+++ Virtual Desktop Assistant/assistant.py
@@ -10,7 +10,7 @@ engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
 # print(voices[1].id)
 engine.setProperty('voice', voices[0].id)
-
+#assistant is getting ready
 
 def speak(audio):
     engine.say(audio)
